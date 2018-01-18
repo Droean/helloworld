a0c80f48a6aaeea95bbb061b00b262220816d987
Droean <33462433+Droean@users.noreply.github.com>
2018-01-18 13:47:27 -0500

    Initial commit first commit

---
+++ src/com/mcfadden/helloworld/Main.java
@@ -0,0 +1,11 @@
+package com.mcfadden.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        // Write your code here
+    }
+}
